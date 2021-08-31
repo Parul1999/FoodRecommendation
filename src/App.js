34f5bc878a6d2f8a5745b603ef9cf103640a229c
Parul <45730946+Parul1999@users.noreply.github.com>
2021-09-01 00:43:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  var allFoods = {
+    "North Indian": [
+      {
+        name: "Rajma Chawal",
+        rating: "5/5"
+      },
+      {
+        name: "Palak Paneer",
+        rating: "4/5"
+      }
+    ],
+    "South Indian": [
+      {
+        name: "Masala Dosa",
+        rating: "5/5"
+      },
+      {
+        name: "Idli",
+        rating: "4.5/5"
+      }
+    ],
+    Chinese: [
+      {
+        name: "Spring Roll",
+        rating: "5/5"
+      },
+      {
+        name: "Quick Noodles",
+        rating: "3.5/5"
+      }
+    ]
+  };
+  const [currentCuisine, setCurrentCuisine] = useState("North Indian");
+
+  function cusineClick(e) {
+    setCurrentCuisine(e.target.value);
+  }
+
+  var allCusine = Object.keys(allFoods);
+  return (
+    <div className="App">
+      <h1>🍽 Food Recommendations </h1>
+      <p>Checkout my favorite cuisines. Select a cuisine to get started</p>
+      {allCusine.map((item) => {
+        return (
+          <button
+            style={
+              item === currentCuisine
+                ? { backgroundColor: "rebeccapurple", color: "white" }
+                : {}
+            }
+            value={item}
+            key={item}
+            onClick={(e) => cusineClick(e)}
+          >
+            {item}
+          </button>
+        );
+      })}
+      <hr />
+      {allFoods[currentCuisine].map((item) => {
+        return (
+          <ul key={item.name}>
+            <li>
+              <div style={{ paddingBottom: "0.4rem" }}> {item.name} </div>
+              <div> {item.rating} </div>
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+}
